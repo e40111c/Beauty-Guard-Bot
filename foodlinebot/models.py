8d6ef3dbc46ba04b5b99d5bbb7bd71ec2870e546
e40111c <e40111c@gmail.com>
2020-08-12 12:33:36 +0800

    ok

--- foodlinebot/models.py
+++ foodlinebot/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 
-        
+class CustomerStatus(models.Model):
+    uid = models.CharField(max_length=255, blank=True, null=True)  # 地區
+    continuous = models.IntegerField(blank=True, null=True)  # 景點名稱
+    nid = models.AutoField(primary_key=True)  # 地址
 
 
         
