5039ea00ba1cad357726c27162b81b2bbdd8a6db
e40111c <44383958+e40111c@users.noreply.github.com>
2020-08-16 17:35:02 +0800

    Update views.py

--- foodlinebot/views.py
+++ foodlinebot/views.py
@@ -125,7 +125,7 @@ def callback(request):
                     message = TemplateSendMessage(
                         alt_text='查無產品',
                         template=ButtonsTemplate(
-                            thumbnail_image_url='https://5b76fcb485d9.ngrok.io/static/carePic_2.jpg',
+                            thumbnail_image_url='https://c4e69d26b7e1.ngrok.io/static/pic/carePic_2.jpg',
                             title='專科',
                             text='沁涼舒活乳',
                             actions=[
