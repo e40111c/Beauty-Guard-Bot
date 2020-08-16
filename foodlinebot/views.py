d76a990dc9df9c521e741895760cd691b035fe4d
e40111c <44383958+e40111c@users.noreply.github.com>
2020-08-16 17:05:26 +0800

    Update views.py

--- foodlinebot/views.py
+++ foodlinebot/views.py
@@ -121,21 +121,22 @@ def callback(request):
                     )
                 elif event.message.text == '710596':
                     message = TemplateSendMessage(
-                        alt_text='掃描QRCode Template無法顯示',
+                        alt_text='查無產品',
                         template=ButtonsTemplate(
-                            thumbnail_image_url='https://c4e69d26b7e1.ngrok.io/static/pic/skincare_02.jpg',
-                            title='Fees 法緻',
-                            text = '沁涼舒活乳',
+                            thumbnail_image_url='https://5b76fcb485d9.ngrok.io/static/skincare_02.jpg',
+                            title='Fees 法緻,
+                            text='沁涼舒活乳',
                             actions=[
-                               MessageTemplateAction(
-                                    label='確認商品', text='完成紀錄'
+                                MessageTemplateAction(
+                                    label='確認儲存', text='儲存完成'
                                 )
                                 MessageTemplateAction(
-                                    label='取消紀錄', text='完成紀錄'
+                                    label='取消儲存', text='儲存完成'
                                 )
                             ]
                         )
                     )
+
 
                 elif event.message.text == '掃描產品條碼':
                     message = TemplateSendMessage(
