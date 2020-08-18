f63dff9e7d842eba6a320a50970936f91c0c8795
e40111c <44383958+e40111c@users.noreply.github.com>
2020-08-18 16:16:48 +0800

    Update urls.py

--- my_django_project/urls.py
+++ my_django_project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from foodlinebot import views
-from foodlinebot.views import hello_view
+from foodlinebot.views import *
 from django.conf.urls import url
 from django.conf import settings
 from django.views.static import serve
