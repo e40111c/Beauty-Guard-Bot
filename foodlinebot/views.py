ffcf459f2316c1e8591c65aabfcef09b64cb7273
e40111c <44383958+e40111c@users.noreply.github.com>
2020-08-24 16:58:49 +0800

    Update views.py

--- foodlinebot/views.py
+++ foodlinebot/views.py
@@ -553,11 +553,11 @@ def message_continuous(countin, uid, userMessage):
     
     elif countin == 8:
        item = str(qrcode_detail(userMessage))
-       message = TemplateSendMessage(
-          alt_text='Confirm template',
-          template=ConfirmTemplate(
-              text='請確認回覆以下是否為您的發票明細:\n' + item,
-              actions=[
+        message = TemplateSendMessage(
+            alt_text='Confirm template',
+            template=ConfirmTemplate(
+                text='請確認回覆以下是否為您的發票明細:\n' + item,
+                actions=[
                     MessageTemplateAction(
                         label='確認',
                         text='儲存商品'
@@ -569,7 +569,7 @@ def message_continuous(countin, uid, userMessage):
                 ]
             )
         )
-        Temp.objects.create(uid=uid,pname='item')
+        Temp.objects.create(uid=uid,pname=str(item))
         updatestate(uid,0,0)
     
     elif countin == 9:
