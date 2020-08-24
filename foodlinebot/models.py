bddfa17a15948a56ae942bff8747d9da316735c2
e40111c <44383958+e40111c@users.noreply.github.com>
2020-08-24 09:43:24 +0800

    Update models.py

--- foodlinebot/models.py
+++ foodlinebot/models.py
@@ -35,16 +35,24 @@ class Product(models.Model):
         
 
         
-class UserProduct(models.Model):
-    uid = models.TextField(default=None, blank=True, null=True)
-    pname = models.TextField(default=None, blank=True, null=True)
-    pbrand = models.TextField(default=None, blank=True, null=True)
-    fit_prod = models.TextField(default=None, blank=True, null=True)  # Field name made lowercase.
-    unfit_prod = models.TextField(default=None, blank=True, null=True)  # Field name made lowercase.
-    
+class User_Product(models.Model):
+    uid = models.TextField()
+    wait_prod = models.TextField(default=None, blank=True, null=True)
+    unfit_prod = models.TextField(default=None, blank=True, null=True)
+    fit_prod = models.TextField(default=None, blank=True, null=True)
+    picurl = models.TextField(default=None, blank=True, null=True)
+    ingredient = models.TextField(default=None, blank=True, null=True)
+    acne = models.TextField(default=None, blank=True, null=True)
+    pchar = models.TextField(default=None, blank=True, null=True)
+    dalton = models.TextField(default=None, blank=True, null=True)
+    safeness = models.TextField(default=None, blank=True, null=True)
+    stimulation = models.TextField(default=None, blank=True, null=True)
+    score = models.TextField(default=None, blank=True, null=True)
+    ptype = models.TextField(default=None, blank=True, null=True)
+    suitable = models.TextField(default=None, blank=True, null=True)
 
     class Meta:
-        db_table = 'User_Product'
+        db_table = "UserProduct"
 
 class CosmeticIngredient(models.Model):
     pname = models.TextField(default=None, blank=True, null=True)
@@ -58,24 +66,18 @@ class CosmeticIngredient(models.Model):
 
     class Meta:
         db_table = 'Cosmetic_Ingredient'    
-        
-class Picture(models.Model):
-    uid = models.TextField(default=None, blank=True, null=True)
-    skincare = models.TextField(default=None, blank=True, null=True)
-    foundation = models.TextField(default=None, blank=True, null=True)
-    cosmetic = models.TextField(default=None, blank=True, null=True)
     
-
-    class Meta:
-        db_table = "Cosemetic_Picture"
-        
 
 class Temp(models.Model):
     uid = models.TextField(default=None, blank=True, null=True)
     pname = models.TextField(default=None, blank=True, null=True)
     price = models.IntegerField(default=None, blank=True, null=True)
     brand = models.TextField(default=None, blank=True, null=True)
+    product = models.TextField(default=None, blank=True, null=True)
+    userproduct = models.TextField(default=None, blank=True, null=True)
+    userkind = models.TextField(default=None, blank=True, null=True)
+
 
     class Meta:
-        db_table = "Temp_p" 
+        db_table = "Temp_p"
 
